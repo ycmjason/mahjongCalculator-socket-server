7ecfb845819e5d985031c7078fc476cc877ba85d
Jason Yu <ycm.jason@gmail.com>
2016-01-18 01:32:09 +0000

    fix origin

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use(function(req, res, next){
+  res.setHeader('Access-Control-Allow-Origin', 'http://www.ycmjason.com');
+  res.setHeader('Access-Control-Allow-Origin', 'http://www.doc.ic.ac.uk');
+});
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
