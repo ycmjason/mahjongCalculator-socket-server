151aad494f5c5d90b8cdfa0e79c3715678447389
Jason Yu <ycm.jason@gmail.com>
2016-01-19 17:51:40 +0000

    fix bug for emitting update mjdata

--- controller.js
+++ controller.js
@@ -6,7 +6,7 @@ exports.onConnection = function(socket){
   console.log('A client, '+socket.id+', has connected!');
 
   socket.on('new game', function(mjData){
-    console.log(socket.id+' created a new game.');
+    console.log('A client, '+socket.id+' created a new game.');
 
     var game = gameTable.addGame(mjData);
     game.addSocket(socket);
@@ -17,25 +17,29 @@ exports.onConnection = function(socket){
   socket.on('update game', function(g){
     var game = gameTable.findGameByCode(g.code);
     game.setMjData(g.mjData);
-    game.emit(g.mjData);
+    game.emit('update mjData', g.mjData);
   });
 
   socket.on('join game', function(code){
-    console.log(socket.id+' joined '+code+'.');
     var game = gameTable.findGameByCode(code);
     if(game==undefined){
       var msg = 'The code you provided is not found.';
       return socket.emit('[fail] join game', msg);
     }
+    console.log('A client, '+socket.id+' joined '+code+'.');
     game.addSocket(socket);
     socket.emit('update mjData', game.getMjData());
     game.emit('update game userNumber', game.getNumberOfSockets());
   });
+
+  socket.on('reconnect', function(){
+    console.log('A client, '+socket.id+' reconnected.');
+  });
   socket.on('disconnect', function(){
     var game = gameTable.findGameBySocket(socket);
     gameTable.socketDisconnect(socket);
-    console.log(socket.id+' disconnected.');
-    if(game){
+    console.log('A client, '+socket.id+' disconnected.');
+    if(game != undefined){
       game.emit('update game userNumber', game.getNumberOfSockets());
     }
   });
